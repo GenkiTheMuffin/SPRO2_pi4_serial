4d3d11a7972cff700801e045588b7ffc56257c28
Patrick <89006255+GenkiTheMuffin@users.noreply.github.com>
2026-05-20 14:35:06 +0200

    Update send_serial.c

--- send_serial.c
+++ send_serial.c
@@ -16,7 +16,7 @@
 #include <errno.h>
 
 /* ── Configuration ─────────────────────────────────────────────────────── */
-#define SERIAL_PORT "/dev/ttyACM0"
+#define SERIAL_PORT "/dev/serial/by-id/usb-Raspberry_Pi_Pico_026AB59ED014A442-if00"
 #define BAUD_RATE    B115200
 #define BUF_SIZE     256
 
