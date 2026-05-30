441188a3647a0588f9bccc69311d1387e0a5fc58
hyggemanden <alexandervestergaard@gmail.com>
2026-05-30 17:06:20 +0200

    Added rotation to translator.py

--- translator.py
+++ translator.py
@@ -1,10 +1,9 @@
 import sys
 import csv
 
-
+#Constants
 STUD_HEIGHT = 9.6 
 STUD_TO_STUD = 8
-
 
 # Reference positions 
 PICK_UP_POSITION = [316,25,-1]
@@ -33,17 +32,6 @@ def init_gcode(gcode_file):
 
 
 def parse_csv(row, gcode_file):
-    #SUDO:
-    #Find pick up location based on brick type
-    #hover over pickup location
-    #pick up
-    #Go up to currentbuild layer+2
-    #If rotate needed: TODO
-        #different XY needed
-    #Go to XY of place location    
-    #Go down place 
-    #Go up and away from plate
-
     type_ = row['Type']
     x = int(row['X'])
     y = int(row['Y'])
@@ -51,15 +39,13 @@ def parse_csv(row, gcode_file):
     r = bool(row['R'])
     
     #Finding pick up location based on brick type
-    local_pick_up_position =   [PICK_UP_POSITION[0],PICK_UP_POSITION[1]+type_offset(type_),PICK_UP_POSITION[2]]
-    
-  
+    pick_up_pos = [PICK_UP_POSITION[0],PICK_UP_POSITION[1]+type_offset(type_),PICK_UP_POSITION[2]]
 
-    #hover over pickup location
-    pick_up_brick(local_pick_up_position, gcode_file)
+    #Hover over pickup location
+    pick_up_brick(pick_up_pos, gcode_file)
     place_brick(type_,x,y,z,r,gcode_file)
 
-
+#Goes to predetermined position and picks up a brick 
 def pick_up_brick(loc_pos, gcode_file):
     gcode_file.write(';Pick up brick\n')
     gcode_file.write('ROTATE_HOME\n')
@@ -75,10 +61,19 @@ def pick_up_brick(loc_pos, gcode_file):
     gcode_file.write(f'G1 X{loc_pos[0]} Y{loc_pos[1]} Z{loc_pos[2]+2*STUD_HEIGHT}\n\n')
 
 
+
+#Translating stud coordinates into machine coordinates. Then places brick
 def place_brick(type_,x,y,z,r,gcode_file):
-    X = ORIGIN_2x2_POSITION[0] - x*STUD_TO_STUD
-    Y = ORIGIN_2x2_POSITION[1] + y*STUD_TO_STUD + type_offset(type_)
-    Z = ORIGIN_2x2_POSITION[2] + z*STUD_HEIGHT
+    if(r == 0):
+        X = ORIGIN_2x2_POSITION[0] - x*STUD_TO_STUD
+        Y = ORIGIN_2x2_POSITION[1] + y*STUD_TO_STUD + type_offset(type_)
+        Z = ORIGIN_2x2_POSITION[2] + z*STUD_HEIGHT
+    else:
+        X = ORIGIN_2x2_ROTATED_POSITION[0] - x*STUD_TO_STUD - type_offset(type_)
+        Y = ORIGIN_2x2_ROTATED_POSITION[1] + y*STUD_TO_STUD 
+        Z = ORIGIN_2x2_ROTATED_POSITION[2] + z*STUD_HEIGHT
+        gcode_file.write(f'ROTATE 90\n')
+
 
     
     gcode_file.write(';Place brick\n')
@@ -94,7 +89,7 @@ def place_brick(type_,x,y,z,r,gcode_file):
     gcode_file.write(f'G1 X{X} Y{Y} Z{Z+2*STUD_HEIGHT} F1500\n')
     gcode_file.write(f'G1 X{PICK_UP_POSITION[0]} Y{PICK_UP_POSITION[1]}\n\n')
     
-
+#Calculates the gripper offset depending on the type of brick (in mm)
 def type_offset(type_):
     match type_:
         case "2x2":
